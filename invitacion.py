43b673be5d08347a2d21c22e308591d3918cda6a
Martin Alvarez <kongosanin@gmail.com>
2026-07-05 13:52:39 -0600

    Add files via upload

--- invitacion.py
+++ invitacion.py
@@ -1,16 +1,25 @@
-import webbrowser
-import time
+import pywhatkit
 import pandas as pd
-import urllib.parse
+import time
 
-df = pd.read_csv('lista.csv') # Asegúrate que tu CSV sea ID,Nombre,Telefono,Confirmado...
+# Cargar Excel
+df = pd.read_csv('lista.csv')
+url_base = "https://martinalvan.github.io/Ary-Martin/?id="
 
-url_base = "https://tu-github-link.github.io/index.html?id="
+print("ADVERTENCIA: Deja el manos libres. El script controlará tu teclado.")
+time.sleep(20)
 
-print("Preparando invitaciones... Abre WhatsApp Web primero.")
-time.sleep(10)
-
-for idx, f in df.iterrows():
-    m = f"¡Qué onda {f['Nombre']}! 👰🤵 Ary y Martin te enviamos esto porque ¡nos casamos! Nuestra historia resumida y tu invitación digital acá: {url_base}{f['ID']}\n🇲🇽 Te esperamos en esta noche mexicana especial. Confírmanos antes de caducar la invitación."
-    webbrowser.open(f"https://web.whatsapp.com/send?phone={f['Telefono']}&text={urllib.parse.quote(m)}")
-    time.sleep(15)
+for index, row in df.iterrows():
+    nombre = row['Nombre']
+    tel = "+" + str(row['Telefono']) # Requiere el signo + y código de país (Ej: +521...)
+    link = url_base + str(row['ID'])
+    
+    mensaje = f"¡Hola {nombre}! 👰🤵 Ary y Martin te enviamos tu invitación digital {link} \n🇲🇽 Te esperamos en esta noche mexicana especial. Confírmanos antes de caducar la invitación."
+    #mensaje = f"¡Qué onda {f['Nombre']}! 👰🤵 Ary y Martin te enviamos esto porque ¡nos casamos! Nuestra historia resumida y tu invitación digital acá: {url_base}{f['ID']}\n🇲🇽 Te esperamos en esta noche mexicana especial. Confírmanos antes de caducar la invitación."
+   
+    
+    # sendwhatmsg_instantly(teléfono, mensaje, tiempo_espera, cerrar_pestaña)
+    # espera 15 seg para que cargue y a los 2 seg después de enviar cierra la pestaña
+    pywhatkit.sendwhatmsg_instantly(tel, mensaje, 15, True, 3) 
+    
+    print(f"Mensaje enviado automáticamente a: {nombre}")
